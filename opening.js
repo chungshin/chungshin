bf57b3aabd89e169852d6cfc4e61981e978e17e4
futurezzz <79406446+futurezzz@users.noreply.github.com>
2021-11-09 14:51:07 +0900

    김마리아 뮤지컬 팝업 및 웹툰페이지에 링크 업데이트

---
+++ opening.js
@@ -0,0 +1,35 @@
+const mainBtn = document.querySelector('.next');
+const mainText = document.querySelector('.main--text')
+const boldText = document.querySelector('.bold--text');
+const logo = document.querySelector('.logo');
+const background = document.querySelector('.background');
+const closeBtn = document.querySelector('.close-btn');
+const popupWinow = document.querySelector('.popup');
+let clickCount = 0;
+
+mainBtn.addEventListener('click',() => {
+  clickCount++;
+  // console.log(clickCount)
+  if (clickCount === 1 ){
+    mainText.style.color = 'transparent';
+    setTimeout(() => {
+      boldText.style.display = 'block';
+      boldText.classList.add('appear');
+    },500);
+  } else if (clickCount === 2 ){
+    boldText.style.color = 'transparent';
+    background.style.opacity = 0;
+    setTimeout(() => {
+      // logo.style.opacity = 1;
+      logo.style.display = 'block';
+      logo.classList.add('appear');
+      mainBtn.textContent = '다음으로';
+    },1000);
+  } else {
+    let win = window.open("menu.html","_self");
+  }
+});
+
+closeBtn.addEventListener('click',()=> {
+  popupWinow.style.display = 'none';
+})
